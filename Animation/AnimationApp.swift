1c0d9b5429f4265bd1f683dfd1c957506cc066c0
SeiOmori <sei.omori.22@gmail.com>
2023-03-29 00:16:44 +0900

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by sei-macmini on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
